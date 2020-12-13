1725d44f953ee805e3936c2148131e6c1341437b
lilMyst <75428585+lilMyst@users.noreply.github.com>
2020-12-13 15:42:36 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# SUPPLY-MISSION---2
-SUPPLY MISSION - 2 project done in VS Code.
+# SupplyMission
---
+++ sketch.js
@@ -0,0 +1,75 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.5, isStatic:true});
+	World.add(world, packageBody);
+	
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+	World.add(world, ground);
+	 
+    bowlWall1 = new Bowl (317,550,10,100);
+	bowlWall2 = new Bowl (369,600,100,10);
+	bowlWall3 = new Bowl (417,550,10,100);
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  packageSprite.x= packageBody.position.x;
+  packageSprite.y= packageBody.position.y;
+  bowlWall1.display();
+  bowlWall2.display();
+  bowlWall3.display();
+
+  
+  drawSprites();
+  keyPressed();
+  
+ 
+}
+
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+  Matter.Body.setStatic(packageBody, false)
+
+
+    
+  }
+}
+
+
+
